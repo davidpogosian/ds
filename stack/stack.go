600ade0977f02c7f8b9cdea608c33dc38b72cbf2
david <david@localhost.localdomain>
2024-08-26 13:28:32 -0400

    Added Stack.Copy, removed nil pointer checks from Stack. Added queue.go

--- stack/stack.go
+++ stack/stack.go
@@ -14,22 +14,16 @@ func NewEmpty[T comparable]() *Stack[T] {
 	return &Stack[T]{}
 }
 
-func NewFromSlice[T comparable](slice []T) (*Stack[T], error) {
-	if slice == nil {
-		return nil, fmt.Errorf("Cannot initialize a Stack with a nil slice.")
-	}
+func NewFromSlice[T comparable](slice []T) *Stack[T] {
 	copiedSlice := make([]T, len(slice))
 	copy(copiedSlice, slice)
-	return &Stack[T]{items: copiedSlice}, nil
+	return &Stack[T]{items: copiedSlice}
 }
 
 func (stack *Stack[T]) Pop() (T, error) {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
 	var zeroValue T
-	if stack == nil {
-		return zeroValue, fmt.Errorf("Cannot pop from a nil Stack pointer.")
-	}
 	if len(stack.items) == 0 {
 		return zeroValue, fmt.Errorf("Cannot pop from an empty Stack.")
 	}
@@ -38,87 +32,69 @@ func (stack *Stack[T]) Pop() (T, error) {
 	return last, nil
 }
 
-func (stack *Stack[T]) Push(newItem T) error {
+func (stack *Stack[T]) Push(newItem T) {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
-	if stack == nil {
-		return fmt.Errorf("Cannot push onto a nil Stack pointer.")
-	}
 	stack.items = append(stack.items, newItem)
-	return nil
 }
 
 func (stack *Stack[T]) Peek() (T, error) {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
 	var zeroValue T
-	if stack == nil {
-		return zeroValue, fmt.Errorf("Cannot peek a nil Stack pointer.")
-	}
 	if len(stack.items) == 0 {
 		return zeroValue, fmt.Errorf("Cannot peek an empty Stack.")
 	}
 	return stack.items[len(stack.items) - 1], nil
 }
 
-func (stack *Stack[T]) IsEmpty() (bool, error) {
+func (stack *Stack[T]) IsEmpty() bool {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
-	if stack == nil {
-		return false, fmt.Errorf("Cannot check the emptiness of a nil Stack pointer.")
-	}
-	return len(stack.items) == 0, nil
+	return len(stack.items) == 0
 }
 
-func (stack *Stack[T]) Size() (int, error) {
+func (stack *Stack[T]) Size() int {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
-	if stack == nil {
-		return -1, fmt.Errorf("Cannot get the size a nil Stack pointer.")
-	}
-	return len(stack.items), nil
+	return len(stack.items)
 }
 
-func (stack *Stack[T]) Clear() error {
+func (stack *Stack[T]) Clear() {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
-	if stack == nil {
-		return fmt.Errorf("Cannot clear a nil Stack pointer.")
-	}
 	stack.items = []T{}
-	return nil
 }
 
-func (stack *Stack[T]) Contains(item T) (int, error) {
+func (stack *Stack[T]) Contains(item T) int {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
-	if stack == nil {
-		return -1, fmt.Errorf("Cannot search a nil Stack pointer.")
-	}
 	for i := range stack.items {
 		if stack.items[i] == item {
-			return i, nil
+			return i
 		}
 	}
-	return -1, nil
+	return -1
 }
 
-func (stack *Stack[T]) ToSlice() ([]T, error) {
+func (stack *Stack[T]) ToSlice() []T {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
-	if stack == nil {
-		return nil, fmt.Errorf("Cannot convert a nil Stack pointer to a slice.")
-	}
 	copiedSlice := make([]T, len(stack.items))
 	copy(copiedSlice, stack.items)
-	return copiedSlice, nil
+	return copiedSlice
+}
+
+func (stack *Stack[T]) Copy() *Stack[T] {
+	stack.mutex.Lock()
+	defer stack.mutex.Unlock()
+	copiedSlice := make([]T, len(stack.items))
+	copy(copiedSlice, stack.items)
+	return NewFromSlice(copiedSlice)
 }
 
 func (stack *Stack[T]) String() string {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
-	if stack == nil {
-		return "nil Stack pointer"
-	}
 	return fmt.Sprintf("%v", stack.items)
 }
