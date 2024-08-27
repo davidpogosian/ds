4b58f645fbc0b5fa3796b403fd37c725309bb833
david <david@localhost.localdomain>
2024-08-27 17:23:53 -0400

    Added sequential tests for the Stack

--- stack/stack.go
+++ stack/stack.go
@@ -10,17 +10,20 @@ type Stack[T comparable] struct {
 	mutex sync.Mutex
 }
 
+// Creates a new empty Stack.
 func NewEmpty[T comparable]() *Stack[T] {
 	return &Stack[T]{}
 }
 
-// also shallow
+// Creates a new Stack from a slice. The slice is copied.
 func NewFromSlice[T comparable](slice []T) *Stack[T] {
 	copiedSlice := make([]T, len(slice))
 	copy(copiedSlice, slice)
 	return &Stack[T]{items: copiedSlice}
 }
 
+// Removes and returns the top item off of the Stack.
+// Returns an error if the Stack is empty.
 func (stack *Stack[T]) Pop() (T, error) {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
@@ -33,12 +36,15 @@ func (stack *Stack[T]) Pop() (T, error) {
 	return last, nil
 }
 
+// Adds a new item to the top of the Stack.
 func (stack *Stack[T]) Push(newItem T) {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
 	stack.items = append(stack.items, newItem)
 }
 
+// Returns the top item from the Stack.
+// Returns an error if the Stack is empty.
 func (stack *Stack[T]) Peek() (T, error) {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
@@ -49,24 +55,29 @@ func (stack *Stack[T]) Peek() (T, error) {
 	return stack.items[len(stack.items) - 1], nil
 }
 
+// Returns bool indicating if the Stack is empty.
 func (stack *Stack[T]) IsEmpty() bool {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
 	return len(stack.items) == 0
 }
 
+// Returns the the number of items in the Stack.
 func (stack *Stack[T]) Size() int {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
 	return len(stack.items)
 }
 
+// Removes all items from the Stack.
 func (stack *Stack[T]) Clear() {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
 	stack.items = []T{}
 }
 
+// Returns nonnegative int indicating poistion of item in the Stack.
+// Returns -1 if the item could not be found.
 func (stack *Stack[T]) Contains(item T) int {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
@@ -78,7 +89,7 @@ func (stack *Stack[T]) Contains(item T) int {
 	return -1
 }
 
-// shallow
+// Returns the Stack as a slice.
 func (stack *Stack[T]) ToSlice() []T {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
@@ -87,7 +98,8 @@ func (stack *Stack[T]) ToSlice() []T {
 	return copiedSlice
 }
 
-// Shallow copy, should I implement deep copy?
+// Returns a copy of the Stack.
+// The Stack's underlying slice is copied, then returned.
 func (stack *Stack[T]) Copy() *Stack[T] {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
@@ -96,6 +108,7 @@ func (stack *Stack[T]) Copy() *Stack[T] {
 	return &Stack[T]{items: copiedSlice}
 }
 
+// Returns a string representation of the Stack.
 func (stack *Stack[T]) String() string {
 	stack.mutex.Lock()
 	defer stack.mutex.Unlock()
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -5,7 +5,13 @@ import (
 	"testing"
 )
 
-func same(sliceA []int, sliceB []int) error {
+func assert[T comparable](t *testing.T, varname string, expected T, got T) {
+	if expected != got {
+		t.Fatalf("Expected '%s' to be: %v, instead got: %v", varname, expected, got)
+	}
+}
+
+func compareSlices[T comparable](sliceA []T, sliceB []T) error {
 	if (len(sliceA) != len(sliceB)) {
 		return fmt.Errorf("SliceA and SliceB are of different lengths.")
 	}
@@ -17,22 +23,186 @@ func same(sliceA []int, sliceB []int) error {
 	return nil
 }
 
-func TestPush(t *testing.T) {
-	stack := NewEmpty()
-	slice := []int{1,2,3}
-	for i := range slice {
-		stack.Push(slice[i])
-	}
-	sliceFromStack, err := stack.ToSlice()
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = same(slice, sliceFromStack)
-	if err != nil {
-		t.Fatal(err)
-	}
+func TestNewEmpty(t *testing.T) {
+	s := NewEmpty[int]()
+	assert(t, "s.Size()", 0, s.Size())
+	assert(t, "s.String()", "[]", s.String())
+}
+
+func TestNewFromSlice(t *testing.T) {
+	t.Run("InitializedSlice", func(t *testing.T) {
+		slice := []int{1, 2, 3}
+		s := NewFromSlice(slice)
+		err := compareSlices(slice, s.ToSlice())
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("NilSlice", func(t *testing.T) {
+		var slice []float64
+		s := NewFromSlice(slice)
+		assert(t, "s.Size()", 0, s.Size())
+		assert(t, "s.String()", "[]", s.String())
+	})
+
+	t.Run("ModifySlice", func(t *testing.T) {
+		originalSlice := []int{1, 2, 3}
+		slice := []int{1, 2, 3}
+		s := NewFromSlice(slice)
+		slice[2] = 99
+		err := compareSlices(originalSlice, s.ToSlice())
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
 }
 
 func TestPop(t *testing.T) {
+	t.Run("EmptyStack", func(t *testing.T) {
+		s := NewEmpty[int]()
+		_, err := s.Pop()
+		if err == nil {
+			t.Fatal("Popped from empty stack")
+		}
+	})
 
+	t.Run("NonemptyStack", func(t *testing.T) {
+		s := NewEmpty[int]()
+		s.Push(1)
+		top, err := s.Pop()
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert(t, "top", 1, top)
+		assert(t, "s.Size()", 0, s.Size())
+	})
+}
+
+func TestPush(t *testing.T) {
+	t.Run("EmptyStack", func(t *testing.T) {
+		s := NewEmpty[int]()
+		s.Push(1)
+		err := compareSlices([]int{1}, s.ToSlice())
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("NonemptyStack", func(t *testing.T) {
+		s := NewFromSlice([]int{1, 2})
+		s.Push(3)
+		err := compareSlices([]int{1, 2, 3}, s.ToSlice())
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+}
+
+func TestPeek(t *testing.T) {
+	t.Run("EmptyStack", func(t *testing.T) {
+		s := NewEmpty[int]()
+		_, err := s.Peek()
+		if err == nil {
+			t.Fatal("Peeked empty stack")
+		}
+	})
+
+	t.Run("NonemptyStack", func(t *testing.T) {
+		s := NewFromSlice([]int{1, 2})
+		two, err := s.Peek()
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert(t, "two", 2, two)
+		assert(t, "s.Size()", 2, s.Size())
+	})
+}
+
+func TestIsEmpty(t *testing.T) {
+	t.Run("EmptyStack", func(t *testing.T) {
+		s := NewEmpty[int]()
+		assert(t, "s.IsEmpty()", true, s.IsEmpty())
+	})
+
+	t.Run("NonemptyStack", func(t *testing.T) {
+		s := NewFromSlice([]int{1, 2, 3})
+		assert(t, "s.IsEmpty()", false, s.IsEmpty())
+	})
+}
+
+func TestSize(t *testing.T) {
+	t.Run("EmptyStack", func(t *testing.T) {
+		s := NewEmpty[int]()
+		assert(t, "s.Size()", 0, s.Size())
+	})
+
+	t.Run("NonemptyStack", func(t *testing.T) {
+		s := NewFromSlice([]int{1, 2, 3})
+		assert(t, "s.Size()", 3, s.Size())
+	})
+}
+
+func TestClear(t *testing.T) {
+	t.Run("EmptyStack", func(t *testing.T) {
+		s := NewEmpty[int]()
+		s.Clear()
+		assert(t, "s.Size()", 0, s.Size())
+	})
+
+	t.Run("NonemptyStack", func(t *testing.T) {
+		s := NewFromSlice([]int{1, 2, 3})
+		s.Clear()
+		assert(t, "s.Size()", 0, s.Size())
+	})
+}
+
+func TestContains(t *testing.T) {
+	t.Run("Exists", func(t *testing.T) {
+		s := NewFromSlice([]int{1, 2, 3})
+		one := s.Contains(2)
+		assert(t, "one", 1, one)
+	})
+
+	t.Run("DoesntExist", func(t *testing.T) {
+		s := NewFromSlice([]int{1, 2, 3})
+		negativeOne := s.Contains(1099)
+		assert(t, "negativeOne", -1, negativeOne)
+	})
+}
+
+func TestToSlice(t *testing.T) {
+	t.Run("Basic", func(t *testing.T) {
+		originalSlice := []int{1, 2, 3}
+		s := NewFromSlice(originalSlice)
+		slice := s.ToSlice()
+		err := compareSlices(originalSlice, slice)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("ModifyStack", func(t *testing.T) {
+		originalSlice := []int{1, 2, 3}
+		s := NewFromSlice(originalSlice)
+		slice := s.ToSlice()
+		slice[2] = 99
+		three, err := s.Pop()
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert(t, "three", 3, three)
+	})
+}
+
+func TestCopy(t *testing.T) {
+	s1 := NewFromSlice([]int{1, 2, 3})
+	s2 := s1.Copy()
+	s1.Pop()
+	assert(t, "s2.Size()", 3, s2.Size())
+}
+
+func TestString(t *testing.T) {
+	s1 := NewFromSlice([]int{1, 2, 3})
+	assert(t, "s1.String()", "[1 2 3]", s1.String())
 }
