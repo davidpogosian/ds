1198f6fe78a73267b2e67e6ea6612079e7c4f490
david <david@localhost.localdomain>
2024-08-28 12:37:28 -0400

    Changed Queue to use ring slice

--- queue/queue.go
+++ queue/queue.go
@@ -9,12 +9,13 @@ type Queue[T comparable] struct {
 	items []T
 	front int
 	rear int
+	size int
 	mutex sync.Mutex
 }
 
 // Creates a new empty Queue.
 func NewEmpty[T comparable]() *Queue[T] {
-	return &Queue[T]{}
+	return &Queue[T]{items: make([]T, 4)}
 }
 
 // Creates a new Queue from a slice.
@@ -25,23 +26,42 @@ func NewFromSlice[T comparable](slice []T) *Queue[T] {
 	return &Queue[T]{
 		items: copiedSlice,
 		front: 0,
-		rear: len(copiedSlice),
+		rear: 0,
+		size: len(copiedSlice),
 	}
+}
+
+func (queue *Queue[T]) grow() {
+	newCapacity := len(queue.items) * 2
+	newItems := make([]T, newCapacity)
+	if queue.front < queue.rear {
+        copy(newItems, queue.items[queue.front:queue.rear])
+    } else {
+        copy(newItems, queue.items[queue.front:])
+        copy(newItems[len(queue.items) - queue.front:], queue.items[:queue.rear])
+    }
+	queue.front = 0
+	queue.rear = queue.size
+	queue.items = newItems
 }
 
 // Adds an item to the rear of the Queue.
 func (queue *Queue[T]) Enqueue(newItem T) {
 	queue.mutex.Lock()
 	defer queue.mutex.Unlock()
-	queue.items = append(queue.items, newItem)
-	queue.rear++
+	if queue.size == len(queue.items) {
+		queue.grow()
+	}
+	queue.items[queue.rear] = newItem
+	queue.rear = (queue.rear + 1) % len(queue.items)
+	queue.size++
 }
 
 // Returns a bool indicating whether or not the Queue is empty.
 func (queue *Queue[T]) IsEmpty() bool {
 	queue.mutex.Lock()
 	defer queue.mutex.Unlock()
-	return queue.front == queue.rear
+	return queue.size == 0
 }
 
 // Removes and returns the item at the front of the Queue.
@@ -49,12 +69,13 @@ func (queue *Queue[T]) IsEmpty() bool {
 func (queue *Queue[T]) Dequeue() (T, error) {
 	queue.mutex.Lock()
 	defer queue.mutex.Unlock()
-	var zeroValue T
-	if queue.front == queue.rear {
+	if queue.size == 0 {
+		var zeroValue T
 		return zeroValue, fmt.Errorf("Cannot dequeue from an empty Queue.")
 	}
 	first := queue.items[queue.front]
-	queue.front++
+	queue.front = (queue.front + 1) % len(queue.items)
+	queue.size--
 	return first, nil
 }
 
@@ -64,7 +85,7 @@ func (queue *Queue[T]) Peek() (T, error) {
 	queue.mutex.Lock()
 	defer queue.mutex.Unlock()
 	var zeroValue T
-	if queue.front == queue.rear {
+	if queue.size == 0 {
 		return zeroValue, fmt.Errorf("Cannot peak an empty Queue.")
 	}
 	first := queue.items[queue.front]
@@ -75,16 +96,16 @@ func (queue *Queue[T]) Peek() (T, error) {
 func (queue *Queue[T]) Size() int {
 	queue.mutex.Lock()
 	defer queue.mutex.Unlock()
-	return queue.rear - queue.front
+	return queue.size
 }
 
 // Removes all items from the Queue.
 func (queue *Queue[T]) Clear() {
 	queue.mutex.Lock()
 	defer queue.mutex.Unlock()
-	queue.items = []T{}
 	queue.front = 0
 	queue.rear = 0
+	queue.size = 0
 }
 
 // Returns a nonnegative int indicating the position of the item in the Queue.
@@ -92,10 +113,12 @@ func (queue *Queue[T]) Clear() {
 func (queue *Queue[T]) Contains(item T) int {
 	queue.mutex.Lock()
 	defer queue.mutex.Unlock()
-	for i := range queue.items {
+	traversed := 0
+	for i := queue.front; traversed != queue.size; i = (i + 1) % len(queue.items) {
 		if queue.items[i] == item {
-			return i
+			return traversed
 		}
+		traversed++
 	}
 	return -1
 }
@@ -110,6 +133,7 @@ func (queue *Queue[T]) Copy() *Queue[T] {
 		items: copiedSlice,
 		front: queue.front,
 		rear: queue.rear,
+		size: queue.size,
 	}
 }
 
@@ -117,8 +141,13 @@ func (queue *Queue[T]) Copy() *Queue[T] {
 func (queue *Queue[T]) ToSlice() []T {
 	queue.mutex.Lock()
 	defer queue.mutex.Unlock()
-	copiedSlice := make([]T, queue.rear - queue.front)
-	copy(copiedSlice, queue.items[queue.front:queue.rear])
+	copiedSlice := make([]T, queue.size)
+	if queue.front < queue.rear {
+        copy(copiedSlice, queue.items[queue.front:queue.rear])
+    } else {
+        copy(copiedSlice, queue.items[queue.front:])
+        copy(copiedSlice[len(queue.items) - queue.front:], queue.items[:queue.rear])
+    }
 	return copiedSlice
 }
 
@@ -126,5 +155,12 @@ func (queue *Queue[T]) ToSlice() []T {
 func (queue *Queue[T]) String() string {
 	queue.mutex.Lock()
 	defer queue.mutex.Unlock()
-	return fmt.Sprintf("%v", queue.items[queue.front:queue.rear])
+	if queue.size == 0 {
+		return "[]"
+	}
+	if queue.front < queue.rear {
+        return fmt.Sprintf("%v", queue.items[queue.front:queue.rear])
+	}
+	concatenated := append(queue.items[queue.front:], queue.items[:queue.rear]...)
+	return fmt.Sprintf("%v", concatenated)
 }
